2ce8ac661698078a55852347ec1e148c2d2ef64a
pablo <paarma>
2017-04-22 13:43:05 -0500

    Test fallando

--- proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
+++ proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
@@ -29,6 +29,7 @@ public class NotifyPersonServiceTest {
 		String message = notifyPersonService.notify(person);
 		//Assert
 		System.out.println(message);
-		Assert.assertNotNull(message);
+		//Assert.assertNotNull(message);
+		Assert.assertNull(message);
 	}
 }
