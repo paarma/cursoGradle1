724cc64856a32d25cf3991595cc00519639a1b52
pablo <paarma>
2017-04-22 14:43:22 -0500

    Insidencia sonar

--- proyecto1/src/main/java/co/ceiba/service/EmailService.java
+++ proyecto1/src/main/java/co/ceiba/service/EmailService.java
@@ -3,12 +3,7 @@ package co.ceiba.service;
 public class EmailService {
 	
 	public String sendMail(String message){
-		try {
-			return "Email send succesful with message: "+ message;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
+		return "Email send succesful with message: "+ message;
 	}
 	
 }
